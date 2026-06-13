b39546169faeaebcbe9210c84e7e59327db878fc
DanilZheltikov <hikikomoridaz@gmail.com>
2026-06-13 11:11:59 +0700

    ref(doc_render): update context type to dict and integrate jinja2 environment for rendering

--- app/doc_formater.py
+++ app/doc_formater.py
@@ -1,15 +1,17 @@
-from typing import Any, Mapping
+from typing import Any
 
+import jinja2
 from docxtpl import DocxTemplate
 
 from logger_setup import setup_logger
 from settings import OUTPUT_DIR, TEMPLATE_PATH
+from utils import timestamp_to_date
 
 logger = setup_logger(name=__name__)
 
 
 def doc_render(
-    context: Mapping[str, Any],
+    context: dict[str, Any],
     filename: str = 'Topics.docx'
 ) -> None:
     """Рендерит DOCX из контекста по шаблону и сохраняет файл."""
@@ -17,7 +19,10 @@ def doc_render(
     if not TEMPLATE_PATH.exists():
         raise FileNotFoundError(f'Шаблон не найден: {TEMPLATE_PATH}')
 
+    jinja_env = jinja2.Environment()
+    jinja_env.filters['timestamp_to_date'] = timestamp_to_date
+
     doc = DocxTemplate(TEMPLATE_PATH)
-    doc.render(context)
+    doc.render(context, jinja_env=jinja_env)
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     doc.save(OUTPUT_DIR / filename)
